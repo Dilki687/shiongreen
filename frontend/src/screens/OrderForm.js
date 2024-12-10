e9a2771fc0a806005398aa2e1b76b7f087691735
Chamodi Dilki <chamodidilki44@gmail.com>
2024-12-10 11:28:11 +0530

    Update OrderForm.js

--- frontend/src/screens/OrderForm.js
+++ frontend/src/screens/OrderForm.js
@@ -111,7 +111,7 @@ const OrderForm = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/orders", {
+      const response = await fetch("https://shiongreen-api.vercel.app/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
